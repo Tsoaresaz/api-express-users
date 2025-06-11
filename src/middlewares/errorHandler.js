56a3f05edb17cc7e4f33d57fff9cfc7b49c2a12f
Thiago <tsoaresaz@gmail.com>
2025-06-11 10:45:28 -0300

    adicionado node_env para não emitir a stack de erro em produção

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,13 @@
 function errorHandler(err, req, res, next) {
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(500).json({ message: 'Erro interno do servidor!' });
+  }
+
   console.error(err.stack);
 
-  res.status(500).json({ message: 'Erro interno do servidor!' });
+  res
+    .status(500)
+    .json({ message: 'Erro interno do servidor!', error: err.stack });
 }
 
 module.exports = errorHandler;
